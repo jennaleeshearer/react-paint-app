61bf16089fcb46541bb93b6ad3311f01fd96bddc
Jenna-Lee Shearer <shearerjennalee@gmail.com>
2024-09-05 19:52:58 +0200

    paint app

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# react-paint-app
+# React Paint App
+
+# Setup :hammer:
+`npm install`
+
+`npm start`
+
+## Production :house:
+Hosted on Netlify
+
+`npm run build`
---
+++ src/App.js
@@ -0,0 +1,72 @@
+import { useEffect, useRef, useState } from "react";
+import Menu from "./components/Menu";
+import "./App.css";
+
+function App() {
+    const canvasRef = useRef(null);
+    const ctxRef = useRef(null);
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [lineWidth, setLineWidth] = useState(5);
+    const [lineColor, setLineColor] = useState("black");
+    const [lineOpacity, setLineOpacity] = useState(0.1);
+
+    useEffect(() => {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.globalAlpha = lineOpacity;
+        ctx.strokeStyle = lineColor;
+        ctx.lineWidth = lineWidth;
+        ctxRef.current = ctx;
+    }, [lineColor, lineOpacity, lineWidth]);
+
+    const startDrawing = (e) => {
+        ctxRef.current.beginPath();
+        ctxRef.current.moveTo(
+            e.nativeEvent.offsetX,
+            e.nativeEvent.offsetY
+        );
+        setIsDrawing(true);
+    };
+
+    const endDrawing = () => {
+        ctxRef.current.closePath();
+        setIsDrawing(false);
+    };
+
+    const draw = (e) => {
+        if (!isDrawing) {
+            return;
+        }
+        ctxRef.current.lineTo(
+            e.nativeEvent.offsetX,
+            e.nativeEvent.offsetY
+        );
+
+        ctxRef.current.stroke();
+    };
+
+    return (
+        <div className="App w-100 d-flex flex-column justify-content-start align-items-center">
+            <h1 className="my-4">React Paint App</h1>
+            <div className="draw-area position-relative border">
+                <Menu
+                    setLineColor={setLineColor}
+                    setLineWidth={setLineWidth}
+                    setLineOpacity={setLineOpacity}
+                />
+                <canvas
+                    onMouseDown={startDrawing}
+                    onMouseUp={endDrawing}
+                    onMouseMove={draw}
+                    ref={canvasRef}
+                    width={"1000px"}
+                    height={"500px"}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default App;
