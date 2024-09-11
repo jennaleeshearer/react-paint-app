d23b75da20269a3fb02453bbf1e8f6c073cd8a70
Jenna-Lee Shearer <shearerjennalee@gmail.com>
2024-09-11 15:45:58 +0200

    bootstrap edits

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,32 +4,21 @@ import "../App.css";
 const Menu = ({ setLineColor, setLineWidth,
     setLineOpacity }) => {
     return (
-        <div className="Menu d-flex mx-auto mt-3 align-items-center rounded justify-content-evenly">
-            <label>Brush Color </label>
-            <input
-                type="color"
-                onChange={(e) => {
-                    setLineColor(e.target.value);
-                }}
-            />
-            <label>Brush Width </label>
-            <input
-                type="range"
-                min="3"
-                max="20"
-                onChange={(e) => {
-                    setLineWidth(e.target.value);
-                }}
-            />
-            <label>Brush Opacity</label>
-            <input
-                type="range"
-                min="1"
-                max="100"
-                onChange={(e) => {
-                    setLineOpacity(e.target.value / 100);
-                }}
-            />
+        <div className="d-flex flex-column align-items-center justify-content-center">
+            <div className="Menu d-flex mx-auto align-items-center rounded justify-content-evenly position-absolute bottom-0">
+                <label>Brush Color </label>
+                <input
+                    type="color"
+                    onChange={(e) => {
+                        setLineColor(e.target.value);
+                    }}
+                />
+                <label>Brush Width </label>
+                <input type="range" className="form-range w-25" min="1" max="20" onChange={(e) => {setLineWidth(e.target.value);}} />
+
+                <label>Brush Opacity</label>
+                <input type="range" className="form-range w-25" min="1" max="20" onChange={(e) => {setLineOpacity(e.target.value / 100);}} />
+            </div>
         </div>
     );
 };
